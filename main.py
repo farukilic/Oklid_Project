fda26297453ddb6b879af686e50d7447b0025458
farukilic <farukkilicaga@gmail.com>
2024-05-01 13:34:05 +0300

    oklid

---
+++ main.py
@@ -0,0 +1,11 @@
+def hypotenuse(x1, y1, x2, y2):
+    fark_x = x1 - x2
+    fark_y = y1 - y2
+    fark = (fark_x**2 + fark_y**2)**(1/2)
+    return fark
+
+uzunluk = hypotenuse(0,12,5,0)
+print(uzunluk)
+
+
+print(31)
