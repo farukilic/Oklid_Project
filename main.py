f7491f38a25e4a2f7dc8d014c050146de8ab5bb4
farukilic <farukkilicaga@gmail.com>
2024-05-03 19:01:14 +0300

    Oklid_Hesaplayıcı

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-def hypotenuse(x1, y1, x2, y2):
-    fark_x = x1 - x2
-    fark_y = y1 - y2
+def eucledianDistance(x1, y1, x2, y2):
+    x1 = float(input("x1 değerini giriniz: "))
+    y1 = float(input("y1 değerini giriniz: "))
+    x2 = float(input("x2 değerini giriniz: "))
+    y2 = float(input("y2 değerini giriniz: "))
+    fark_x = x2 - x1
+    fark_y = y2 - y1
     fark = (fark_x**2 + fark_y**2)**(1/2)
     return fark
-
-uzunluk = hypotenuse(0,16,8,0)
-print(uzunluk)
-print(31)
-
-
-
-
-
+uzunluk = eucledianDistance(0,16,8,0)
+Distance = []
+Distance.append(uzunluk)
+print(Distance)
